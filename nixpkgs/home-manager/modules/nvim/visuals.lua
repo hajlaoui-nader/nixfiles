1ccaa4fbf16d5b08e1a13883f337dd56318f7497
hajlaoui-nader <hajlaoui.nader@gmail.com>
2023-03-02 15:21:14 +0100

    fix visuals

--- nixpkgs/home-manager/modules/nvim/visuals.lua
+++ nixpkgs/home-manager/modules/nvim/visuals.lua
@@ -2,8 +2,8 @@ require'lspkind'.init()
  -- highlight error: https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
  vim.wo.colorcolumn = "99999"
  vim.opt.list = true
- vim.opt.listchars:append({ eol = "↴" })
- vim.opt.listchars:append({ space = "⋅"})
+--  vim.opt.listchars:append({ eol = "" })
+--  vim.opt.listchars:append({ space = "⋅"})
  require("indent_blankline").setup {
     char = "│",
     show_current_context = true,
