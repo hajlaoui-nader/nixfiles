402fcdb28e82c97b3a44b48d35ed9eaca149a075
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2023-06-23 15:16:42 +0200

    enable fish integration for fzf

--- nixpkgs/home-manager/modules/fzf.nix
+++ nixpkgs/home-manager/modules/fzf.nix
@@ -2,6 +2,7 @@
   programs.fzf = {
       enable = true;
       enableZshIntegration = true;
+      enableFishIntegration = true;
       defaultCommand = "fd --type file --follow"; # FZF_DEFAULT_COMMAND
       defaultOptions = [ "--height 20%" ]; # FZF_DEFAULT_OPTS
       fileWidgetCommand = "fd --type file --follow"; # FZF_CTRL_T_COMMAND
