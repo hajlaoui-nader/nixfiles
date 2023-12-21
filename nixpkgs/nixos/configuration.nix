8376f7db2a7e4bbe79f7deb590a6eb5dd8ac4653
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2023-12-21 11:05:42 +0100

    add gpu amd

--- nixpkgs/nixos/configuration.nix
+++ nixpkgs/nixos/configuration.nix
@@ -118,6 +118,9 @@
     #  wget
   ];
 
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
