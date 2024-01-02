5727082dd2da5763ef82387415aa7099569584a6
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2024-01-02 14:04:00 +0100

    add tmux conf

--- nixpkgs/home-manager/programs/tmux/default.nix
+++ nixpkgs/home-manager/programs/tmux/default.nix
@@ -1,4 +1,8 @@
 { lib, pkgs, ... }:
+let
+  #plugins = pkgs.tmuxPlugins // pkgs.callPackage ./custom-plugins.nix { };
+  tmuxConf = lib.readFile ./default.conf;
+in
 {
   programs.tmux = {
     enable = true;
@@ -6,6 +10,24 @@
     baseIndex = 1;
     terminal = "screen-256color";
     keyMode = "vi";
+    escapeTime = 0;
     shortcut = "a";
+
+    extraConfig = tmuxConf;
+    plugins = with pkgs.tmuxPlugins; [
+      cpu
+      nord # theme
+      {
+        plugin = resurrect;
+        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '60' # minutes
+        '';
+      }
+    ];
   };
 }
