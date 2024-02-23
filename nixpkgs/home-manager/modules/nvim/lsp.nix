5e9305ed129e109a6f8b315fcf409bb0931e6a6d
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2024-02-23 11:27:47 +0100

    add html lsp support

--- nixpkgs/home-manager/modules/nvim/lsp.nix
+++ nixpkgs/home-manager/modules/nvim/lsp.nix
@@ -209,5 +209,14 @@
           end,
           cmd = { "${pkgs.nodePackages.typescript-language-server}/bin/typescript-language-server", "--stdio" }
         }
+
+      -- HTML config
+      lspconfig.html.setup {
+          capabilities = capabilities;
+          on_attach = function(client, bufnr)
+            attach_keymaps(client, bufnr)
+          end,
+          cmd = { "${pkgs.nodePackages.vscode-html-languageserver-bin}/bin/html-languageserver", "--stdio" }
+        }
   '';
 }
