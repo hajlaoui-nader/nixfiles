80561cb773ce64f2bfbe639be1bdeb51950d5fd2
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2024-03-28 09:04:28 +0100

    add cachix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,27 @@
 {
   description = "home-manager configuration for linux, mac and raspberry pi";
 
+  # the nixConfig here only affects the flake itself, not the system configuration!
+  nixConfig = {
+    # override the default substituters
+    substituters = [
+      # cache mirror located in China
+      # status: https://mirror.sjtu.edu.cn/
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      # status: https://mirrors.ustc.edu.cn/status/
+      # "https://mirrors.ustc.edu.cn/nix-channels/store"
+
+      "https://cache.nixos.org"
+
+      # nix community's cache server
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      # nix community's cache server public key
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
@@ -33,6 +54,9 @@
               inherit nixpkgs;
             };
           }
+          {
+            nix.settings.trusted-users = [ "zeus" ];
+          }
         ];
       };
     };
@@ -42,13 +66,21 @@
         pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
         modules = [
           ./nixpkgs/home-manager/linux.nix
+          {
+            nix.settings.trusted-users = [ "naderh" ];
+          }
         ];
       };
 
       # nix build .#homeConfigurations.homepi.system
       homepi = inputs.home-manager.lib.homeManagerConfiguration {
         pkgs = inputs.nixpkgs.legacyPackages.aarch64-linux;
-        modules = [ ./nixpkgs/home-manager/homepi.nix ];
+        modules = [
+          ./nixpkgs/home-manager/homepi.nix
+          {
+            nix.settings.trusted-users = [ "naderh" ];
+          }
+        ];
       };
     };
 
@@ -68,6 +100,9 @@
             home-manager.extraSpecialArgs = {
               inherit nixpkgs;
             };
+          }
+          {
+            nix.settings.trusted-users = [ "naderh" ];
           }
         ];
         inputs = { inherit darwin nixpkgs; };
