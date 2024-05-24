aa4cd1cfe2404bca24fd2f45c18ee10d362ccdd5
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2024-05-24 19:34:41 +0200

    add vim/neovim to env packages

--- nixpkgs/nixos/configuration.nix
+++ nixpkgs/nixos/configuration.nix
@@ -119,16 +119,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-    #(pkgs.waybar.overrideAttrs (oldAttrs: {
-    #mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
-    #}))
-    #waybar
-    #dunst
-    #libnotify
-    #rofi-wayland
-    #swww
+    vim
+    neovim
     kitty
     playerctl
     pciutils
