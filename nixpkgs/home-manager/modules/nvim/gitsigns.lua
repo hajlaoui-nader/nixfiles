39233a43e80e82aa449255fb6f8e6f9984dbc090
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2024-08-21 13:33:08 +0200

    fix gitsigns

--- nixpkgs/home-manager/modules/nvim/gitsigns.lua
+++ nixpkgs/home-manager/modules/nvim/gitsigns.lua
@@ -1,29 +1,5 @@
 local wk = require("which-key")
 require("gitsigns").setup({
-	--keymaps = {
-	--noremap = true,
-
-	--['n <leader>gn'] = { expr = true, "&diff ? \'\' : '<cmd>Gitsigns next_hunk<CR>'"},
-	--['n <leader>gp'] = { expr = true, "&diff ? \'\' : '<cmd>Gitsigns prev_hunk<CR>'"},
-
-	--['n <leader>gs'] = '<cmd>Gitsigns stage_hunk<CR>',
-	--['v <leader>gs'] = ':Gitsigns stage_hunk<CR>',
-	--['n <leader>gu'] = '<cmd>Gitsigns undo_stage_hunk<CR>',
-	--['n <leader>gr'] = '<cmd>Gitsigns reset_hunk<CR>',
-	--['v <leader>gr'] = ':Gitsigns reset_hunk<CR>',
-	--['n <leader>gR'] = '<cmd>Gitsigns reset_buffer<CR>',
-	--['n <leader>gb'] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
-	--['n <leader>gS'] = '<cmd>Gitsigns stage_buffer<CR>',
-	--['n <leader>gU'] = '<cmd>Gitsigns reset_buffer_index<CR>',
-	--['n <leader>gts'] = ':Gitsigns toggle_signs<CR>',
-	--['n <leader>gtn'] = ':Gitsigns toggle_numhl<CR>',
-	--['n <leader>gtl'] = ':Gitsigns toggle_linehl<CR>',
-	--['n <leader>gtw'] = ':Gitsigns toggle_word_diff<CR>',
-
-	---- Text objects
-	--['o ih'] = ':<C-U>Gitsigns select_hunk<CR>',
-	--['x ih'] = ':<C-U>Gitsigns select_hunk<CR>'
-	--},
 	on_attach = function(bufnr)
 		local gs = package.loaded.gitsigns
 
@@ -58,14 +34,6 @@ require("gitsigns").setup({
 			{ "<leader>g", group = "gitsigns" },
 			---- blame gb
 			{ "<leader>gb", "<cmd>lua require'gitsigns'.blame_line{full=true}<cr>", desc = "blame (full)" },
-			-- toggle current line blame
-			{
-				"<leader>gtb",
-				"<cmd>Gitsigns toggle_current_line_blame<cr>",
-				desc = "toggle current line blame",
-			},
-			-- toggle deleted
-			{ "<leader>gtd", "<cmd>Gitsigns toggle_deleted<cr>", desc = "toggle deleted" },
 			-- diff this
 			{ "<leader>gd", "<cmd>Gitsigns diffthis<cr>", desc = "diff current file" },
 			-- diff this ~
@@ -74,7 +42,23 @@ require("gitsigns").setup({
 			{ "<leader>gs", "<cmd>lua require'gitsigns'.stage_buffer()<cr>", desc = "stage buffer" },
 			-- R
 			{ "<leader>gr", "<cmd>lua require'gitsigns'.reset_buffer_index()<cr>", desc = "reset buffer" },
-		})
+		
+    })
+
+    -- toggle
+    wk.add({
+      mode ="n",
+      {"<leader>gt", group ="toggle"},
+      -- toggle current line blame
+			{
+				"<leader>gtb",
+				"<cmd>Gitsigns toggle_current_line_blame<cr>",
+				desc = "toggle current line blame",
+			},
+			-- toggle deleted
+			{ "<leader>gtd", "<cmd>Gitsigns toggle_deleted<cr>", desc = "toggle deleted" },
+
+    })
 
 		-- git hunks
 		wk.add({
