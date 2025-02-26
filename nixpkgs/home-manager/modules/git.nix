9dbc9b06a3e4e3d639880883752efd1df350d9cf
include-nader-h <nader@echo-analytics.io>
2025-02-26 12:04:33 +0100

    better git

--- nixpkgs/home-manager/modules/git.nix
+++ nixpkgs/home-manager/modules/git.nix
@@ -3,7 +3,7 @@
   programs.git = {
     enable = true;
     userName = "Nader Hajlaoui";
-    userEmail = "hajlaoui.nader@gmail.com";
+    userEmail = "nader@echo-analytics.io";
 
     delta = {
       enable = true;
@@ -31,13 +31,27 @@
     extraConfig = {
       pull.rebase = true;
       init.defaultBranch = "main";
-      github.user = "hajlaoui-nader";
+      github.user = "include-nader-h";
 
       push.autoSetupRemote = true;
+      push.followTags = true; # push tags when pushing branches
 
       core.editor = "nvim";
       core.fileMode = false;
       core.ignorecase = false;
+      column.ui = "auto";
+      branch.sort = "-committerdate"; # sort branches by last commit date
+      tag.sort = "version:refname"; # sort tags by version number
+      diff.algorithm = "histogram";
+      diff.colorMoved = "plain"; # don't color moved lines
+      diff.mnemonicprefix = true; # use mnemonic prefixes in diffs 
+      diff.renames = true; # detect renames
+      # fetch 
+      fetch.prune = true; # prune deleted branches 
+      fetch.pruneTags = true; # prune deleted tags 
+      fetch.all = true; # fetch all branches
+      # commit 
+      commit.verbose = true; # show diff in commit message
     };
   };
 }
