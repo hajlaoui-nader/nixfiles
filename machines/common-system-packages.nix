84916c027c1c07303766ecdb3038dc9b76479732
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2025-04-29 16:31:49 +0200

    format code

--- machines/common-system-packages.nix
+++ machines/common-system-packages.nix
@@ -8,14 +8,10 @@
   ];
 
   # Configure registry to match flake inputs
-  nix.registry = lib.mapAttrs
-    (name: flake: { inherit flake; })
-    inputs;
+  nix.registry = lib.mapAttrs (name: flake: { inherit flake; }) inputs;
 
   # Set nix path to match inputs
-  nix.nixPath = lib.mapAttrsToList
-    (name: value: "${name}=${value}")
-    inputs;
+  nix.nixPath = lib.mapAttrsToList (name: value: "${name}=${value}") inputs;
 
   nix.channel.enable = false;
 }
