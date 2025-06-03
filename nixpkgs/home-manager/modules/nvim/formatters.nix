5199d6ad4683375e837254232e6d3fcb3e2afffc
Nader Hajlaoui <nader.hajlaoui@vizzia.fr>
2025-06-03 17:22:27 +0200

    add typescript formatter

--- nixpkgs/home-manager/modules/nvim/formatters.nix
+++ nixpkgs/home-manager/modules/nvim/formatters.nix
@@ -5,21 +5,23 @@
         noremap = true,
         silent = true,
     })
-    
+
     require("conform").setup({
-    
+
       format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
         lsp_fallback = true,
       },
-    
+
       formatters_by_ft = {
         python = { "black" },
         lua = { "stylua" },
         html = { "prettier" },
         css = { "prettier" },
         markdown = { "prettier" },
+        typescript = { "prettier" },
+        javascript = { "prettier" },
         -- Use the "_" filetype to run formatters on filetypes that don't have other formatters configured.
         ["_"] = { "trim_whitespace" },
       },
