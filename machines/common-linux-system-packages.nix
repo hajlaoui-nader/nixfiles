6f088fa6788f15a1914a291b12f857b8b22fefec
Nader Hajlaoui <nader.hajlaoui@vizzia.fr>
2025-08-05 14:38:29 +0200

    register appimage

--- machines/common-linux-system-packages.nix
+++ machines/common-linux-system-packages.nix
@@ -18,4 +18,14 @@
   programs.nix-ld.enable = true;
   programs.appimage.enable = true;
   programs.appimage.binfmt = true;
+
+  # register AppImage support
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
 }
