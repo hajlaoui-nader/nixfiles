f66388a58bb4a2df708d39e937ce3382bb3591b6
Nader Hajlaoui <nader.hajlaoui@vizzia.fr>
2025-08-05 14:15:51 +0200

    use appimage

--- machines/common-linux-system-packages.nix
+++ machines/common-linux-system-packages.nix
@@ -9,11 +9,13 @@
     pkgs.vlc # media player
     pkgs.feh # image viewer
     pkgs.smile # emoji picker
-    pkgs.appimage-run
+    #pkgs.appimage-run
     # set when darwin gets updated, and ghostty is no more marked as broken
     #pkgs.ghostty
   ];
 
   # allow dynamic linked packages to be used
   programs.nix-ld.enable = true;
+  programs.appimage.enable = true;
+  programs.appimage.binfmt = true;
 }
