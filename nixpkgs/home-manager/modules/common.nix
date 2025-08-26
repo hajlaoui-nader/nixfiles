dab8e1215d9e511a6725de8599b7481eea2dc0ad
Nader Hajlaoui <hajlaoui.nader@gmail.com>
2025-08-26 18:58:52 +0200

    add gdb

--- nixpkgs/home-manager/modules/common.nix
+++ nixpkgs/home-manager/modules/common.nix
@@ -14,42 +14,44 @@ in
   programs.direnv.nix-direnv.enable = true;
 
   home.packages = [
-      pkgs.man-pages # linux programmer's manual
-      pkgs.zip # archives
-      pkgs.unzip # archives
-      pkgs.lsd
-      pkgs.docker
-      pkgs.jq
-      pkgs.docker-compose # docker manager
-      pkgs.neofetch # command-line system information
-      pkgs.ripgrep # fast grep
-      pkgs.tree # display files in a tree view
-      pkgs.eza # a better `ls`
-      pkgs.bottom # a better `top`
-      pkgs.tree-sitter # syntax highlighting
-      pkgs.fd # a better `find`
-      #lf # a file manager
-      pkgs.file # file type
-      pkgs.xxd # hexdump
-      pkgs.nmap
-      pkgs.duf # disk usage
-      pkgs.lua-language-server
-      pkgs.lua
-      pkgs.go
-      pkgs.gcc14
-      pkgs.dig # DNS lookup
-      pkgs.openresolv # DNS resolver
-      unstable.claude-code
-    ] ++ lib.optionals pkgs.stdenv.isDarwin [
-      pkgs.coreutils # provides `dd` with --status=progress
-      pkgs.wifi-password
-    ] ++ lib.optionals pkgs.stdenv.isLinux [
-      pkgs.iputils # provides `ping`, `ifconfig`, ...
-      pkgs.libuuid # `uuidgen` (already pre-installed on mac)
-      pkgs.font-awesome # awesome fonts
-      pkgs.material-design-icons # fonts with glyphs
-      pkgs.vscode
-    ];
+    pkgs.man-pages # linux programmer's manual
+    pkgs.zip # archives
+    pkgs.unzip # archives
+    pkgs.lsd
+    pkgs.docker
+    pkgs.jq
+    pkgs.docker-compose # docker manager
+    pkgs.neofetch # command-line system information
+    pkgs.ripgrep # fast grep
+    pkgs.tree # display files in a tree view
+    pkgs.eza # a better `ls`
+    pkgs.bottom # a better `top`
+    pkgs.tree-sitter # syntax highlighting
+    pkgs.fd # a better `find`
+    #lf # a file manager
+    pkgs.file # file type
+    pkgs.xxd # hexdump
+    pkgs.nmap # network scanner
+    pkgs.duf # disk usage
+    pkgs.lua-language-server
+    pkgs.lua
+    pkgs.go
+    pkgs.gcc14
+    pkgs.gdb
+    pkgs.inetutils # provides `ftp`, `telnet`, ...
+    pkgs.dig # DNS lookup
+    pkgs.openresolv # DNS resolver
+    unstable.claude-code
+  ] ++ lib.optionals pkgs.stdenv.isDarwin [
+    pkgs.coreutils # provides `dd` with --status=progress
+    pkgs.wifi-password
+  ] ++ lib.optionals pkgs.stdenv.isLinux [
+    pkgs.iputils # provides `ping`, `ifconfig`, ...
+    pkgs.libuuid # `uuidgen` (already pre-installed on mac)
+    pkgs.font-awesome # awesome fonts
+    pkgs.material-design-icons # fonts with glyphs
+    pkgs.vscode
+  ];
 
   home.sessionVariables = {
     DISPLAY = ":0";
